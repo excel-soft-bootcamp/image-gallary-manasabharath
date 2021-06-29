bf8d372689ae4fcd558e0bd237753301f45b35e6
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-06-29 14:09:14 +0530

    Add files via upload

---
+++ ImageGalleryApp/ImageGallery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ImageGalleryApp
+{
+    public class ImageGallery
+    {
+        IShare share;
+        public void Share(IShare share)
+        {
+            share.Send();
+
+        }
+    }
+}
+
---
+++ ImageGalleryApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace ImageGalleryApp
+{
+    static void Main(string[] args)
+    {
+        Gmail _mailAccount = new Gmail();
+        ImageGallery _Gallery = new ImageGallery();
+        _Gallery.Share(_mailAccount);
+
+            Bluetooth _blueToothDevice = new Bluetooth();
+        _Gallery.Share(_blueToothDevice);
+
+            WhatsUp _messanger = new WhatsUp();
+        _Gallery.Share(_messanger);        
+    }
+}
